77f2a7ba37a8b8d9255a3f8beef4763d899adf98
Ali M <alimousavi-01@outlook.com>
2022-05-22 14:20:47 +0430

    Created Client Api

---
+++ src/Services/Client/Client/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapPost("/send", ([FromQuery] string msg) =>
+{
+    return msg;
+})
+.WithName("SendMsg");
+
+app.Run();
