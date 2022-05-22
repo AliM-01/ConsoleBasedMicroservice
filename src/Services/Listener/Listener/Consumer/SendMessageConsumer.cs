9e73bad2236b607b82e932eeb416c9c2192a1d71
Ali M <alimousavi-01@outlook.com>
2022-05-22 19:46:10 +0430

    Created SendMessageConsumer

---
+++ src/Services/Listener/Listener/Consumer/SendMessageConsumer.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Text.Json;
+using EventBus.RabbitMQ.Constants;
+using EventBus.RabbitMQ.Events;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Listener.Consumer;
+
+public class SendMessageConsumer
+{
+    private readonly ConnectionFactory _factory;
+
+    public SendMessageConsumer(ConnectionFactory factory)
+    {
+        _factory = factory;
+    }
+
+    public async Task Consume()
+    {
+        using (IConnection connection = _factory.CreateConnection())
+        {
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: EventBusConstants.SendMessageQueue,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                var result = channel.BasicGet(EventBusConstants.SendMessageQueue, true);
+                if (result != null)
+                {
+                    string message = Encoding.UTF8.GetString(e.Body.Span);
+                    var sendMessageEvent = JsonSerializer.Deserialize<SendMessageEvent>(message);
+
+                    await Console.Out.WriteLineAsync($"{sendMessageEvent?.Message} was sent to {sendMessageEvent?.Email}");
+                }
+            }
+        }
+    }
+}
--- src/Services/Listener/Listener/Program.cs
+++ src/Services/Listener/Listener/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddEnvironmentVariables();
+
+var config = builder.Build();
+
+Console.WriteLine("-------------------\n Listener service is up ! \n-------------------\n Press any key to exit...\n\n");
+
+await Task.Run(() => Console.ReadKey());
