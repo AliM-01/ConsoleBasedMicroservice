6309653d6bd698f97bef41554036ec046dde8525
Ali M <alimousavi-01@outlook.com>
2022-05-22 14:49:46 +0430

    Implemented SendMessage in Client

--- src/Services/Client/Client/Program.cs
+++ src/Services/Client/Client/Program.cs
@@ -1,6 +1,28 @@
+using EventBus.RabbitMQ;
+using EventBus.RabbitMQ.Constants;
+using EventBus.RabbitMQ.Producers;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IRabbitMQConnection>(sp =>
+{
+    var factory = new ConnectionFactory()
+    {
+        HostName = builder.Configuration["EventBus:HostName"]
+    };
+
+    if (!string.IsNullOrEmpty(builder.Configuration["EventBus:UserName"]))
+    {
+        factory.UserName = builder.Configuration["EventBus:UserName"];
+        factory.Password = builder.Configuration["EventBus:Password"];
+    }
+
+    return new RabbitMQConnection(factory);
+});
+
+builder.Services.AddSingleton<ISendMessageProducer, SendMessageProducer>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,9 +34,13 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-app.MapPost("/send", ([FromQuery] string msg) =>
+app.MapPost("/send", ([FromQuery] string msg, [FromQuery] string email, [FromServices] ISendMessageProducer producer) =>
 {
-    return msg;
+    var publishModel = new EventBus.RabbitMQ.Events.SendMessageEvent(msg, email);
+
+    producer.PublishSendMessage(EventBusConstants.SendMessageQueue, publishModel);
+
+    return Results.Ok;
 })
 .WithName("SendMsg");
 
