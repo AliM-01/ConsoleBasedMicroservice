271571ffa2788c269b4c8f99bcaf8662b117aea8
Ali M <alimousavi-01@outlook.com>
2022-05-22 14:41:19 +0430

    Implemented SendMessageProducer

--- src/BuildingBlocks/EventBus.RabbitMQ/Producers/SendMessageProducer.cs
+++ src/BuildingBlocks/EventBus.RabbitMQ/Producers/SendMessageProducer.cs
@@ -1,4 +1,37 @@
-﻿namespace EventBus.RabbitMQ.Producers;
+﻿using EventBus.RabbitMQ.Events;
+
+namespace EventBus.RabbitMQ.Producers;
 public class SendMessageProducer
 {
+    private readonly IRabbitMQConnection _connection;
+
+    public SendMessageProducer(IRabbitMQConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public Task PublishSendMessage(string queueName, SendMessageEvent publishModel)
+    {
+        using (var channel = _connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            string message = System.Text.Json.JsonSerializer.Serialize(publishModel);
+            byte[] body = System.Text.Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+            properties.DeliveryMode = 2;
+
+            channel.ConfirmSelect();
+            channel.BasicPublish(exchange: "", routingKey: queueName, mandatory: true,
+                    basicProperties: properties, body: body);
+            channel.WaitForConfirmsOrDie();
+
+            channel.ConfirmSelect();
+        }
+
+        return Task.CompletedTask;
+    }
+
 }
